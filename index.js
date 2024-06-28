fb54172b9797bf1dd24e82ce0eb67b949b8279f5
Faktor 10 <oreillyross@gmail.com>
2024-06-28 14:18:55 +0000

    change res from json to text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.json({ Message: "Hello from Vanilla JS!" });
+  res.send("Hello from Vanilla JS!");
 });
 
 app.listen(3000, () => {
