6c911a8555c13aa5c086abecb1581cab0a017fd0
Faktor 10 <oreillyross@gmail.com>
2024-06-28 14:21:57 +0000

    change port to 80

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Vanilla JS!");
 });
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(80, () => {
+  console.log("listening on 80");
 });
